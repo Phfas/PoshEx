e54573b73636fdeefb3c6eed074c4c4dfe6f9b7f
Phfas <phfas@outlook.com>
2018-12-12 23:19:45 -0700

    Added another execute method using PSCommand param

--- PoshEx/PoshEx/MainWindow.xaml.cs
+++ PoshEx/PoshEx/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Management.Automation;
+using System.Management.Automation.Runspaces;
 using System.Collections.ObjectModel;
 
 namespace PoshEx {
@@ -20,11 +21,15 @@ namespace PoshEx {
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
+        Runspace _runspace = RunspaceFactory.CreateRunspace();
         PowerShell _poshInstance = PowerShell.Create();
         //IAsyncResult _asyncResult;
 
         public MainWindow() {
             InitializeComponent();
+            //_poshInstance.Runspace = _runspace;
+
+            _runspace.Open();
 
         }
 
@@ -46,6 +51,26 @@ namespace PoshEx {
 
         }
 
+        private string GetPoshInstanceCommandString(PSCommand psCommands) {
+            StringBuilder commandString = new StringBuilder();
+            foreach (var item in psCommands.Commands) {
+                if (item.CommandText != "Out-String") {
+                    if (string.IsNullOrWhiteSpace(commandString.ToString()) == false) {
+                        commandString.Append(" | ");
+                    }
+                    commandString.Append(item.ToString());
+
+                    if (item.Parameters != null) {
+                        foreach (var paramItem in item.Parameters) {
+                            commandString.Append($" -{paramItem.Name.ToString()} {paramItem.Value.ToString()}");
+                        }
+                    }
+                }
+            }
+            return commandString.ToString();
+        }
+
+
         private void ExecuteSynchronous(string script) {
              
             _poshInstance.AddScript($"{script} | Out-String -Stream");
@@ -61,11 +86,43 @@ namespace PoshEx {
 
             SetOutput("", true);
         }
-
         private void ExecuteAsynchronous(string script) {
             _poshInstance.Streams.ClearStreams();
             _poshInstance.Commands.Clear();
+            //_poshInstance2.Commands = script;
+            //_poshInstance2.AddCommand("Out-String").AddParameter("stream");
             _poshInstance.AddScript($"{script} | Out-String -Stream");
+
+
+            PSDataCollection<PSObject> outputCollection = new PSDataCollection<PSObject>();
+            outputCollection.DataAdded -= outputCollection_DataAdded;
+            outputCollection.DataAdded += outputCollection_DataAdded;
+
+            // the streams (error,debug,progress, etc) are available on the poshInstance
+            // we can review them during or after execution
+            // we can also be notified when a new item is written to the stream (like this):
+            _poshInstance.Streams.Progress.DataAdded -= Progress_DataAdded;
+            _poshInstance.Streams.Verbose.DataAdded -= Verbose_DataAdded;
+            _poshInstance.Streams.Error.DataAdded -= Error_DataAdded;
+
+            _poshInstance.Streams.Progress.DataAdded += Progress_DataAdded;
+            _poshInstance.Streams.Verbose.DataAdded += Verbose_DataAdded;
+            _poshInstance.Streams.Error.DataAdded += Error_DataAdded;
+
+            //begin invoke execution on the pipeline
+            // use this overload to specify an output stream buffer
+            IAsyncResult asyncResult = _poshInstance.BeginInvoke<PSObject, PSObject>(null, outputCollection);
+
+            SetOutput(_poshInstance.Commands.Commands[0].ToString(), false);
+
+
+        }
+
+        private void ExecuteAsynchronous(PSCommand script) {
+            _poshInstance.Streams.ClearStreams();
+            _poshInstance.Commands.Clear();
+            _poshInstance.Commands = script;
+            _poshInstance.AddCommand("Out-String").AddParameter("stream");
 
             PSDataCollection<PSObject> outputCollection = new PSDataCollection<PSObject>();
             outputCollection.DataAdded -= outputCollection_DataAdded;
@@ -86,10 +143,11 @@ namespace PoshEx {
             // use this overload to specify an output stream buffer
             IAsyncResult asyncResult = _poshInstance.BeginInvoke<PSObject, PSObject>(null,outputCollection);
 
-            SetOutput(_poshInstance.Commands.Commands[0].ToString(), false);
+            SetOutput(GetPoshInstanceCommandString(script), false);
 
 
         }
+
 
         private void Verbose_DataAdded(object sender, DataAddedEventArgs e) {
             this.Dispatcher.Invoke(() => {
@@ -132,6 +190,18 @@ namespace PoshEx {
 
         private void Button_Click_3(object sender, RoutedEventArgs e) {
             _poshInstance.BeginStop(null,null);
+
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e) {
+            PSCommand command = new PSCommand().AddCommand(textboxScript.Text).AddParameter("Path",textboxParam1.Text);
+            ExecuteAsynchronous(command);
+
+        }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e) {
+            PSCommand command = new PSCommand().AddCommand(textboxScript.Text).AddParameter("Path", textboxParam1.Text).AddParameter("File");
+            ExecuteAsynchronous(command);
 
         }
     }
